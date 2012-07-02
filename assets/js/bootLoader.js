929b4ef9b971aadff4a183990c438669aa71b08b
Travis Love <love18942000@gmail.com>
2012-07-01 17:15:36 -0700

    Working, WAMI 2.0

--- assets/js/bootLoader.js
+++ assets/js/bootLoader.js
@@ -1,40 +1,60 @@
 $(document).ready(function(){
 	
-	//Normalize browser getUserMedia call
-	navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia;
-	
-	//Normalize URL for video/audio stream
-	window.URL || (window.URL = window.webkitURL || window.msURL || window.oURL);
-	
-	//Browser HTML5 feature detection
-	function hasGetUserMedia(){
-		return !!(navigator.getUserMedia);
-	};
-	
-	//Load camera/mic streams
-	function loadUserMedia(useVideo,useAudio,outputID){
-		
-		//if HTML5 supported
-		if(hasGetUserMedia()){
-			
-			//Send stream to selected video/audio element #id
-			var output = document.getElementById(outputID);
-			navigator.getUserMedia({video: useVideo, audio: useAudio}, successCallback, errorCallback);
-				function successCallback(stream) {
-					output.src = (window.URL && window.URL.createObjectURL) ? window.URL.createObjectURL(stream) : stream;
-					console.log("Stream is a go!");
+var Eleuthera; 
+function loadWAMI() {
+var jsgf = 
+	"#JSGF V1.0;\n" +
+	"grammar Eleuthera;\n" +
+	"public <top> = hello wami | i want a cracker | feed me;\n";
+
+        var grammar = {
+            language : "en-us", 
+            grammar : jsgf
+		};
+
+  	//Handlers are functions which are called for various events:
+  	var options = {
+		devKey : "a6db053ed80bc7624909e622ce5d3a24",
+		guiID : "properties",
+		grammar : grammar,
+		onReady : onWamiReady,
+		onRecognition : onWamiRecognition,
+		onRecognitionResult : onWamiRecognitionResult,
+		onError : onWamiError,
+		onTimeout : onWamiTimeout
+        }; 
+
+    //Create your WAMI application with the settings and grammar we just created
+  	Eleuthera = new Wami.App(options);
+}
+
+var live = $('#live');
+live.hide();
+
+function onWamiReady(){
+	live.append('<p>WAMI successful</p>').addClass('success').slideDown(300).delay(2000).slideUp(300);
+}
+
+function onWamiRecognitionResult(result) {
+			    Wami.utils.debug(result);
+}
+
+function onWamiRecognition(result) {
+			    Wami.utils.debug(result);
+				if(result.settled()) {
+				live.empty().append('<p>' + (result.text()) + '</p>').addClass('success').slideDown(300).delay(2000).slideUp(300);
+				}else{
+				live.empty().append('<p>You said? ' + (result.text()) + '</p>').addClass('caution').slideDown(300).delay(2000).slideUp(300);
 				}
-				function errorCallback(error) {
-					console.error('An error occurred: [CODE ' + error.code + ']');
-					return;
-				}
-		}
-		//Flash fall-back
-		else{
-			Wami.setup({id : outputID});
-		}
-	};
-	
-	
-loadUserMedia(false,false,"wami");
+}
+
+function onWamiError(type, message){
+	live.append('<p>There was a problem loading...</p>').addClass('error').slideDown(300).delay(5000).slideUp(300).empty();
+		console.log("WAMI error: type  = " + type + ", message = " + message);
+}
+function onWamiTimeout() {
+	live.append('<p>WAMI has timed out, sadly you must refresh...</p>').addClass('caution').slideDown(300).delay(2000).slideUp(300).empty();
+}
+
+loadWAMI();
 });
