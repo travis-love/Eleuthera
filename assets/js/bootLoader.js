7cd3fa3db3d81790afbb1b9c6ca682900ae31029
Travis Love <love18942000@gmail.com>
2012-07-02 19:49:21 -0700

    Now responds to basic operation and element cmds
    For Example: "append section"

--- assets/js/bootLoader.js
+++ assets/js/bootLoader.js
@@ -2,14 +2,24 @@ $(document).ready(function(){
 	
 var Eleuthera; 
 function loadWAMI() {
-var jsgf = 
+
+//create the grammar
+var header = 
 	"#JSGF V1.0;\n" +
 	"grammar Eleuthera;\n" +
-	"public <top> = hello wami | i want a cracker | feed me;\n";
-
+	"public <top> = (<command>+ <element>);\n";
+	
+var command = 
+	"<command> = insert | append | prepend | select | deselect | remove | erase | delete;\n";
+	
+var elements = 
+	"<element> = style | link | article | aside | details | caption | figure | footer | header | group | nav | section | function | var;";
+	
         var grammar = {
-            language : "en-us", 
-            grammar : jsgf
+			language : "en-us",
+			grammar : header + command + elements,
+			aggregate : false,
+			incremental : false
 		};
 
   	//Handlers are functions which are called for various events:
@@ -30,9 +40,13 @@ var jsgf =
 
 var live = $('#live');
 live.hide();
+function clean(){
+	setInterval(function(){live.empty().removeClass();} ,5000);
+	}
 
 function onWamiReady(){
 	live.append('<p>WAMI successful</p>').addClass('success').slideDown(300).delay(2000).slideUp(300);
+	clean();
 }
 
 function onWamiRecognitionResult(result) {
@@ -42,18 +56,22 @@ function onWamiRecognitionResult(result) {
 function onWamiRecognition(result) {
 			    Wami.utils.debug(result);
 				if(result.settled()) {
-				live.empty().append('<p>' + (result.text()) + '</p>').addClass('success').slideDown(300).delay(2000).slideUp(300);
+					live.append('<p>' + (result.text()) + '</p>').addClass('success').slideDown(300).delay(2000).slideUp(300);
+					clean();
 				}else{
 				live.empty().append('<p>You said? ' + (result.text()) + '</p>').addClass('caution').slideDown(300).delay(2000).slideUp(300);
+				clean();
 				}
 }
 
 function onWamiError(type, message){
-	live.append('<p>There was a problem loading...</p>').addClass('error').slideDown(300).delay(5000).slideUp(300).empty();
+	live.empty().append('<p>There was a problem loading...</p>').addClass('error').slideDown(300).delay(5000).slideUp(300);
+	clean();
 		console.log("WAMI error: type  = " + type + ", message = " + message);
 }
 function onWamiTimeout() {
-	live.append('<p>WAMI has timed out, sadly you must refresh...</p>').addClass('caution').slideDown(300).delay(2000).slideUp(300).empty();
+	live.empty().append('<p>WAMI has timed out, sadly you must refresh...</p>').addClass('caution').slideDown(300).delay(2000).slideUp(300);
+	clean();
 }
 
 loadWAMI();
