01b9f3ea02ac877769b04e3095791621b63b1ad9
Travis Love <love18942000@gmail.com>
2012-07-30 16:12:42 -0700

    Transition from JSON to using CodeMirror
    Issues with JSON approach lead to transition to WYSIWYG type approach with
    CodeMirror.
    
    WAMI servers went down, so had to globalize functions and use a text input
    and onClick button to send strings to those functions.  Currently postToLive and outputToDoc does not work.

--- assets/js/bootLoader.js
+++ assets/js/bootLoader.js
@@ -1,7 +1,7 @@
-$(document).ready(function(){
+var Ele = {};
 	
 var Eleuthera; 
-function loadWAMI() {
+Ele.loadWAMI = function() {
 
 //create the grammar
 var header = 
@@ -10,7 +10,7 @@ var header =
 	"public <top> = (<command> <element> [<audit> <command> <element>]*);\n";
 	
 var commands = 
-	"<command> = (insert | append | prepend | select | deselect | remove | erase | delete) ;\n";
+	"<command> = (insert | append | prepend | select | deselect | remove | erase | delete);\n";
 	
 var elements = 
 	"<element> = (style | link | article | aside | details | caption | figure | footer | header | group | nav | section | function | var);\n";
@@ -39,7 +39,7 @@ var audit =
 
     //Create your WAMI application with the settings and grammar we just created
   	Eleuthera = new Wami.App(options);
-}
+};
 
 
 function onWamiReady(){
@@ -55,9 +55,9 @@ function onWamiRecognition(result) {
 			    Wami.utils.debug(result);
 				var resultString = (result.text());
 				if(result.settled()) {
-					processResults(resultString);
+					Ele.processResults(resultString);
 				}else{
-					postToLive(resultString, "caution");
+					Ele.postToLive(resultString, "caution");
 				}
 }
 
@@ -72,7 +72,7 @@ function onWamiTimeout() {
 
 //DOM global vars
 var live = $('#live');
-live.hide();
+
 var outputString, outputCode;
 
 //contains phonetics and HTML code pairs
@@ -170,53 +170,69 @@ var objElements = {
 //contains the documents data and HTML contents
 var objDocument = {
 	"data" : [
-		{"type" : "html" , "lang" : "en" , "locale" : "en-US" , "charset" : "UTF-8"}
+		{"type" : "html" , "lang" : "en" , "locale" : "en-US" , "charset" : "UTF-8" , "children" : [1,2,3,4]}
 	],
 	"contents" : [
 		{"name" : "dock type" , "codeStart" : "<!DOCTYPE HTML>" , "codeEnd" : "\n" , "objID" : 1 , "parent" : 0 , "hasChild" : false , "processed" : false},
-		{"name" : "H T M L" , "codeStart" : "<html lang=\"\">\n" , "codeEnd" : "\n</html>" , "objID" : 2 , "parent" : 0 , "hasChild" : true , "processed" : false},
+		{"name" : "H T M L" , "codeStart" : "<html lang=\"\">\n" , "codeEnd" : "\n</html>" , "objID" : 2 , "parent" : 0 , "hasChild" : true , "children" : [3.4] , "processed" : false},
 		{"name" : "head" , "codeStart" : "<head>\n" , "codeEnd" : "\n</head>\n" , "objID" : 3 , "parent" : 2 , "hasChild" : false , "processed" : false},
-		{"name" : "body" , "codeStart" : "<body>\n" , "codeEnd" : "\n</body>" , "objID" : 4 , "parent" : 2 , "hasChild" : true , "processed" : false}
+		{"name" : "body" , "codeStart" : "<body>\n" , "codeEnd" : "\n</body>" , "objID" : 4 , "parent" : 2 , "processed" : false}
 	]
 };
 
 //displays the HTML code in the textarea #output
-function displayDoc(){
+Ele.displayDoc = function(){
 outputCode = "";
-	$.each(objDocument.contents, function(i,val){
-		if(objDocument.contents[i].hasChild === true && objDocument.contents[i].processed !== true){
-			outputCode += val.codeStart;
-			$.each(objDocument.contents, function(j,valu){
-				if(objDocument.contents[j].processed !== true && 
-					objDocument.contents[j].parent === objDocument.contents[i].objID){
-					outputCode += (valu.codeStart + valu.codeEnd);
-					objDocument.contents[j].processed = true;
-				}
-			});
-				outputCode += val.codeEnd;
-				objDocument.contents[i].processed = true;
-		}else if(objDocument.contents[i].hasChild === false && objDocument.contents[i].processed !== true){
-			outputCode += (val.codeStart + val.codeEnd);
-			objDocument.contents[i].processed = true;
-		}
-	});
-	$('#output').text(outputCode);
-	$.each(objDocument.contents, function(k,v){
-		objDocument.contents[k].processed = false;
-	});
+$.each(objDocument.contents, function(i,val){
+if(objDocument.contents[i].hasChild === true && objDocument.contents[i].processed !== true){
+outputCode += val.codeStart;
+$.each(objDocument.contents, function(j,valu){
+if(objDocument.contents[j].processed !== true &&
+objDocument.contents[j].parent === objDocument.contents[i].objID){
+outputCode += (valu.codeStart + valu.codeEnd);
+objDocument.contents[j].processed = true;
 }
+});
+outputCode += val.codeEnd;
+objDocument.contents[i].processed = true;
+}else if(objDocument.contents[i].hasChild === false && objDocument.contents[i].processed !== true){
+outputCode += (val.codeStart + val.codeEnd);
+objDocument.contents[i].processed = true;
+}
+});
+$('#code').text(outputCode);
+$.each(objDocument.contents, function(k,v){
+objDocument.contents[k].processed = false;
+});
+};
+
+/* function getChildren(children){
+	var str = "";
+	for(x=0;x<children.length;x++){
+		if(objDocument.contents[(children[x] - 1)].hasChild !== true && objDocument.contents[(children[x] - 1)].processed !== true){
+			str += (objDocument.contents[(children[x] - 1)].codeStart + objDocument.contents[(children[x] - 1)].codeEnd);
+			objDocument.contents[(children[x] - 1)].processed = true;
+		}else if(objDocument.contents[(children[x] - 1)].hasChild === true && objDocument.contents[(children[x] - 1)].processed !== true){
+			str += objDocument.contents[(children[x] - 1)].codeStart;
+			str += getChildren(objDocument.contents[(children[x] - 1)].children);
+			str += objDocument.contents[(children[x] - 1)].codeEnd;
+			objDocument.contents[(children[x] - 1)].processed = true;
+		}
+	
+	}
+	
+	return str;
+} */
 
 //Posts messages and results to ARIA-live
-function postToLive(message, classString){
+Ele.postToLive = function(message, classString){
 	live.append('<p>' + message + '</p>').addClass(classString).slideDown(300).delay(4000).slideUp(300);
 	setInterval(function(){live.empty().removeClass();} ,5000);
-	}
+	};
 
 //Separates words, and finds matches	
-function processResults(resultString){
-	for(var i in resultString){
-		var word = resultString.split(' ');
-	}
+Ele.processResults = function(resultString){
+	var word = resultString.split(' ');
 	var cmd = word[0], ele = word[1], codeStart, codeEnd;
 	switch(cmd){
 		case "insert":
@@ -224,26 +240,26 @@ function processResults(resultString){
 				if(v.name == ele){
 					codeStart = v.codeStart;
 					codeEnd = v.codeEnd;
-					outputToDoc(ele,codeStart,codeEnd);
+					Ele.outputToDoc(ele,codeStart,codeEnd);
 				}
 			});
-			displayDoc();
-			postToLive(resultString, "success");
+			Ele.displayDoc();
+			Ele.postToLive(resultString, "success");
 			break;
 		
 		case "select":
 			$.makeArray(objDocument.contents);
 			var selected = $.inArray(ele, objDocument.contents);
-			postToLive(selected, "caution");
+			Ele.postToLive(selected, "caution");
 			break;
 		
 		default:
-			postToLive(resultString, "success");
+			Ele.postToLive(resultString, "success");
 	}
-}
+};
 
 //Assigns objID, and adds HTML element to doc obj array
-function outputToDoc(name,codeStart,codeEnd){
+Ele.outputToDoc = function(name,codeStart,codeEnd){
 
 	var arrayCount = (objDocument.contents.length);
 	var obj = {"name" : name , "codeStart" : codeStart , "codeEnd" : codeEnd , "objID" : ++arrayCount , "parent" : 4 , "hasChild" : false , "processed" : false};
@@ -251,7 +267,35 @@ function outputToDoc(name,codeStart,codeEnd){
 		
 	}
 	objDocument.contents.push(obj);
-} 
+/* 	objDocument.contents[3].parent.push(arrayCount);
+	objDocument.data[0].children.push(arrayCount); */
+};
 
-loadWAMI();
+//Ele.loadWAMI();
+
+$(document).ready(function(){
+	live.hide();
+	Ele.postToLive("WAMI Loaded", "success");
+	Ele.displayDoc();
+	
+ var delay;
+      // Initialize CodeMirror editor with a nice html5 canvas demo.
+      var editor = CodeMirror.fromTextArea(document.getElementById('code'), {
+        mode: 'text/html',
+        tabMode: 'indent',
+        onChange: function() {
+          clearTimeout(delay);
+          delay = setTimeout(updatePreview, 300);
+        }
+      });
+      
+      function updatePreview() {
+        var previewer = document.getElementById('preview');
+		var preview =  previewer.contentDocument ||  previewer.contentWindow.document;
+        preview.open();
+        preview.write(editor.getValue());
+        preview.close();
+      }
+      setTimeout(updatePreview, 300);
+
 });
